47a35e9c4a5fd32d30b14eed03243826317424a3
axel <151853255+axwii@users.noreply.github.com>
2025-09-03 15:14:53 +0200

    Update email recipient address for order processing

--- src/app/api/sendOrder/route.js
+++ src/app/api/sendOrder/route.js
@@ -92,7 +92,7 @@ export async function POST(req) {
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
-      to: "axel@wiingaard.net", // morten.nielsen@kosangas.dk
+      to: "bestilling@kosan-aalborg.dk", // morten.nielsen@kosangas.dk
       subject: `Ny gasbestilling fra ${companyName}`,
       ...emailContent,
     };
